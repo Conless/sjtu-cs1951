c1e5017439480b99e7be2ec9ba9dc8fadff98145
Conless <conlesspan@outlook.com>
2023-03-06 21:11:00 +0800

    feat(contest): finish the problems of contest 0.

--- contest/0/1794_1.cpp
+++ contest/0/1794_1.cpp
@@ -4,21 +4,27 @@ using namespace std;
 
 const int MAXN = 2e5 + 5;
 
+string s;
 int cnt[2];
 int tr[2][MAXN][26], fail[2][MAXN << 3];
-int ed[MAXN], st[MAXN];
+int st[MAXN], ed[MAXN];
+int st_tag[MAXN], ed_tag[MAXN];
 
 void insert(const string &p) {
-    int u[2] = {0};
-    for (auto ch : p) {
-        for (int i : {0, 1}) {
-            if (!tr[i][u[i]][ch - 'a'])
-                tr[i][u[i]][ch - 'a'] = ++cnt[i];
-            u[i] = tr[i][u[i]][ch - 'a'];
-        }
+    int u = 0;
+    for (int i = 0; i < p.size(); i++) {
+        if (!tr[0][u][p[i] - 'a'])
+            tr[0][u][p[i] - 'a'] = ++cnt[0];
+        u = tr[0][u][p[i] - 'a'];
     }
-    ed[u[0]]++;
-    st[u[1]]++;
+    ed[u]++;
+    u = 0;
+    for (int i = p.size() - 1; i >= 0; i--) {
+        if (!tr[1][u][p[i] - 'a'])
+            tr[1][u][p[i] - 'a'] = ++cnt[1];
+        u = tr[1][u][p[i] - 'a'];
+    }
+    st[u]++;
 }
 
 void build(int opt) {
@@ -29,9 +35,53 @@ void build(int opt) {
             que[++tail] = tr[opt][0][i];
     while (head != tail) {
         int u = que[++head];
-        
+        for (int i = 0; i < 26; i++) {
+            if (tr[opt][u][i]) {
+                fail[opt][tr[opt][u][i]] = tr[opt][fail[opt][u]][i];
+                que[++tail] = tr[opt][u][i];
+            } else {
+                tr[opt][u][i] = tr[opt][fail[opt][u]][i];
+            }
+        }
     }
-    
 }
 
-int main() { return 0; }
+void query() {
+    int u = 0;
+    for (int i = 0; i < s.size(); i++) {
+        u = tr[0][u][s[i] - 'a'];
+        int v = u;
+        while (v) {
+            ed_tag[i] += ed[v];
+            v = fail[0][v];
+        }
+    }
+    u = 0;
+    for (int i = s.size() - 1; i >= 0; i--) {
+        u = tr[1][u][s[i] - 'a'];
+        int v = u;
+        while (v) {
+            st_tag[i] += st[v];
+            v = fail[1][v];
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> s >> n;
+    while (n--) {
+        string t;
+        cin >> t;
+        insert(t);
+    }
+    build(0);
+    build(1);
+    query();
+    long long ans = 0;
+    for (int i = 0; i < s.size() - 1; i++)
+        ans += ed_tag[i] * st_tag[i + 1];
+    cout << ans << endl;
+    return 0;
+}
